1056a069e076302356761d721fa46b3055d4dba6
Valery Tschopp <valery.tschopp@switch.ch>
2010-03-22 08:56:39 +0000

    copyright updated.

--- src/log.c
+++ src/log.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2009 Members of the EGEE Collaboration.
+ * Copyright (c) Members of the EGEE Collaboration. 2008.
  * See http://www.eu-egee.org/partners for details on the copyright holders.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
